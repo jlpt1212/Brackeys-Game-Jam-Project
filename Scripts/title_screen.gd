d2e67d366f99ae0734642b0f8835cb575ab4476c
Jerrick Liu <jlpt1212@gmail.com>
2026-08-27 14:31:26 -0400

    Added rules and more stuff to the title screen
    yippee

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
-func _on_button_pressed() -> void:
+@export var camera: Camera2D
+
+func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Game Scene.tscn")
+
+
+func _on_rules_button_pressed() -> void:
+	camera.position.y += 648
+
+
+func _on_back_button_pressed() -> void:
+	camera.position.y -= 648
