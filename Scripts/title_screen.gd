e77795893f944d7c93c059db926910f364a49b25
Jerrick Liu <jlpt1212@gmail.com>
2026-08-28 22:45:11 -0400

    Added dialogue (mostly) and scenes to game

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var camera: Camera2D
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Game Scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Game Day 1.tscn")
 
 func _on_rules_button_pressed() -> void:
 	camera.position.y += 648
